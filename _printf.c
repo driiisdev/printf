6245f61cf049670a71e99121b8ac57213e536e24
Idris Yakub <olayinkayakub@yahoo.com>
2022-10-26 00:30:36 +0100

    all task

--- _printf.c
+++ _printf.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * _printf - produces output according to a format.
- * @fmt: format string containing the characters.
- * Return: the number of characters printed (excluding the null byte) 
+ * @format: format string containing the characters.
+ * Return: the number of characters printed (excluding the null byte)
  */
 
 int _printf(const char *format, ...)
 {
 int i = 0, j, len = 0, count = 0;
-const char *fmt = format;
 va_list valist;
 print_t func_types[] = {
 {'c', print_char}, {'s', print_string}, {'d', print_int},
@@ -16,47 +15,41 @@ print_t func_types[] = {
 {'R', print_rot13}, {'u', print_unsigned}, {'o', print_oct},
 {'x', print_hex_lower}, {'X', print_hex_upper}, {'p', print_pointer},
 {'S', print_special_chars}};
-
-if (!fmt || (fmt[0] == '%' && fmt[1] == 0))
-{
+if (!format || (format[0] == '%' && format[1] == 0))
 return (-1);
-}
-
-va_start(valist, fmt);
-
-while (fmt && fmt[i])
+va_start(valist, format);
+while (format && format[i])
 {
-if (fmt[i] == '%')
+if (format[i] == '%')
 {
-if (fmt[++i] == '%')
+if (format[++i] == '%')
 {
 len += _putchar('%');
 }
 j = 0;
 while (j <= 12)
 {
-if (fmt[i] == func_types[j].t)
+if (format[i] == func_types[j].t)
 {
 len += func_types[j].f(valist);
 count = 1;
-break; 
+break;
 }
-j++; 
+j++;
 }
-if (!count && fmt[i] != '%')
+if (!count && format[i] != '%')
 {
 len += 2;
 _putchar('%');
-_putchar(fmt[i]); 
+_putchar(format[i]);
 }
 }
 else
 {
-len += _putchar(fmt[i]);
+len += _putchar(format[i]);
 }
 i++;
 }
 va_end(valist);
-
 return (len);
 }
