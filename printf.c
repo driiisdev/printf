63c3a789c104604f20a4dd42ebbec9f1d02e4467
Idris Yakub <olayinkayakub@yahoo.com>
2022-10-26 01:02:06 +0100

    task done

--- printf.c
+++ printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _printf - produces output according to a format.
+ * printf - produces output according to a format.
  * @format: format string containing the characters.
  * Return: the number of characters printed (excluding the null byte)
  */
