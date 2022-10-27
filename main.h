0a51a5df9b200d39a014a721395f53f1700cc98d
Idris Yakub <olayinkayakub@yahoo.com>
2022-10-27 21:22:15 +0100

    printf task

--- main.h
+++ main.h
@@ -21,6 +21,7 @@ int _putchar(char c);
 
 int print_char(va_list c);
 int print_string(va_list s);
+int print_perc(va_list perc);
 
 int print_int(va_list i);
 int print_unsigned(va_list u);
@@ -35,7 +36,6 @@ int print_special_chars(va_list S);
 int print_rev(va_list r);
 int print_rot13(va_list R);
 
-int print_perc(va_list perc);
 
 int _printf(const char *format, ...);
 
